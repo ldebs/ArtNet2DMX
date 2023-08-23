d4ca9b9f73a0f32096ce6dfc55cc405741f757e2
ldebs <ldebs@ldebs.org>
2023-08-24 00:15:54 +0200

    move espDMX

--- lib/espDMX/espDMX.cpp
+++ lib/espDMX/espDMX.cpp
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License along with thi
 If not, see http://www.gnu.org/licenses/
 */
 
-#include "espDMX/espDMX.h"
+#include "espDMX.h"
 
 #include <stdlib.h>
 #include <stdio.h>
